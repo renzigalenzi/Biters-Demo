732f68701f3ceb20e10031f9c5575686aa74367e
Lauren F <lef592@neo.tamu.edu>
2015-03-24 21:48:30 -0500

    Anyone willing to test this?
    I found some other code and modified it, but I can’t get the directory
    to work for the mac. Anyone willing to try this out by entering their
    directory in the Movie script?

---
+++ Demo for Biters/Assets/Scripts/Movie.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class Movie : MonoBehaviour
+{
+	//public Texture2D[] slides = new Texture2D[9];  //this is removed, no need to set the size its auto detected
+	private Texture2D[] slides;
+	
+	private float changeTime = 0.04f;
+	public float framePerSec = 24f;
+	private int currentSlide = 0;
+	private float timeSinceLast = 1.0f;
+	private Texture currTex;
+	private Rect currRec; 
+	
+	public string myPath = @"/Assets/Resources/Panel1"; // directory where all the *.jpg files are that need to be animated
+	public string extention = "png";      //extension you looking for
+	
+	void Start()
+	{
+		Debug.Log ("Finding files....");
+		getFiles(); //new added function
+		
+		if (slides != null)
+		{
+			//calc the time to change from fps
+			changeTime = 1.0f / framePerSec;
+			Debug.Log ("FPS change time is: "+changeTime);
+
+
+			currTex = slides[currentSlide];
+			//currRec = new Rect(-slides[currentSlide].width/2, -slides[currentSlide].height/2, slides[currentSlide].width, slides[currentSlide].height);
+			GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), currTex, ScaleMode.ScaleToFit, true, 2.0f);
+			currentSlide++;
+		}
+		else
+		{
+			Debug.Log ("Set reading directory and file type please");
+		}
+	}
+	
+	void Update()
+	{
+		if (slides != null)
+		{
+			if(timeSinceLast > changeTime  && currentSlide < slides.Length)
+			{
+				currTex = slides[currentSlide];
+				//currRec = new Rect(-slides[currentSlide].width/2, -slides[currentSlide].height/2, slides[currentSlide].width, slides[currentSlide].height);
+				GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), currTex, ScaleMode.ScaleToFit, true, 2.0f);
+				timeSinceLast = 0.0f;
+				currentSlide++;
+			}
+			timeSinceLast += Time.deltaTime;
+			
+			if(currentSlide == slides.Length)
+			{
+				currentSlide = 0;
+			}
+		}
+	}
+	
+	internal void getFiles()
+	{
+		if (System.IO.Directory.Exists(myPath))
+		{
+			DirectoryInfo dir = new DirectoryInfo(myPath);
+			Debug.Log("Looking for files in dir: "+myPath);
+			
+			FileInfo[] info = dir.GetFiles("*."+extention);
+			
+			// Get number of files, and set the length for the texture2d array
+			int totalFiles =  info.Length;
+			slides = new Texture2D[totalFiles];
+			
+			int i = 0;
+			
+			//Read all found files
+			foreach (FileInfo f in info)
+			{
+				string filePath = f.Directory + "/" + f.Name;
+				Debug.Log("["+i+"] file found: "+filePath);
+				
+				var bytes     = System.IO.File.ReadAllBytes(filePath);
+				var tex         = new Texture2D(1, 1);
+				
+				tex.LoadImage(bytes);
+				slides[i] = tex;
+				
+				i++;
+			}
+		}
+		else
+		{
+			Debug.Log ("Directory DOES NOT exist! ");
+		}
+	}
+}
